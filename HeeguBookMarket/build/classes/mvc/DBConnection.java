9c91190b58801a777805c12e51e71f2c365b7c9c
kimheegu <chkorea11@gmail.com>
2024-06-13 17:38:48 +0900

    Add files via upload

---
+++ HeeguBookMarket/build/classes/mvc/DBConnection.java
@@ -0,0 +1,19 @@
+package mvc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	public static Connection getConnection() throws SQLException,ClassNotFoundException{
+		Connection conn = null;
+		
+		String url = "jdbc:mysql://localhost:3306/ShoesMarketDB";
+		String user = "root";
+		String password = "1234";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		conn = DriverManager.getConnection(url,user,password);
+		return conn;
+	}
+}
